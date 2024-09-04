8f31912a37a2e635ad2eaefcfe0f61a1209d367b
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2024-09-03 19:35:10 -0600

    Clase 3 septiembre

--- Assets/Script/S2_MovimientoLocal.cs
+++ Assets/Script/S2_MovimientoLocal.cs
@@ -9,18 +9,20 @@ public class S2_MovimientoLocal : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {        
+        //transform.Rotate(0, 45, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
          if (Input.GetKey(KeyCode.W)){
-            transform.Translate( transform.forward * velocidad * Time.deltaTime );
+            //transform.Translate( transform.forward * velocidad * Time.deltaTime);            
+            transform.position += transform.forward * velocidad * Time.deltaTime;
         }
         else if(Input.GetKey(KeyCode.S)){
-            transform.Translate( transform.forward * velocidad * -1 * Time.deltaTime );
+            //transform.Translate( transform.forward * velocidad * -1 * Time.deltaTime);            
+            transform.position += transform.forward * -1 * velocidad * Time.deltaTime;
         }
 
     }
